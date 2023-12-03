f4219f241f0d397ffdb7312ce05a73eab3818f59
Anastasia <pikovaru@mail.ru>
2023-12-03 13:12:37 +0500

    Edit cardInfo

--- src/Components/CardInfo.js
+++ src/Components/CardInfo.js
@@ -3,15 +3,19 @@ import React, {Component} from "react";
 import '../Styles/CardInfo.css'
 import backArrowIcon from '../Files/BackArrowIcon.svg'
 
+
+// По факту этот объект это все айдишники полей, куда надо вписывать поля. [id объекта]: [Название поля]
 const form = {
-    "fullname:": "ФИО:",
-    "education" : "Учебное заведение:",
-    "speciality" : "Специальность/направление:",
+    "fullname": "ФИО:",
+    "education": "Учебное заведение:",
+    "speciality": "Специальность/направление:",
     "degree": "Академическая степень:",
+    "course": "Курс:",
     "phone": "Номер телефона:",
     "telegram": "Telegram тег:",
     "vk": "Vk id:",
     "email": "Email:",
+    //test-result: Результаты тестирования
 
 }
 
@@ -28,12 +32,12 @@ class CardInfo extends Component{
                 </section>
                 <section className="card-information">
                     {Object.keys(form).map(key => (
-                        <div>
+                        <div className={"div-"+key}>
                             <p>{form[key]}</p>
                             <p className="info" id={key}>Заглушка</p>
                         </div>
                     ))}
-                    <div className="flex-box">
+                    <div className="flex-box div-about-me">
                         <div>
                             <p>О себе:</p>
                             <p className="about-me"></p>
@@ -41,7 +45,7 @@ class CardInfo extends Component{
                         <div className="flex-box center vertical">
                             <div className="flex-box vertical test-info">
                                 <p>Результаты тестирования практиканта</p>
-                                <p className="info">34/40</p>
+                                <p className="info" id="test-result">34/40</p>
                             </div>
                             <div className="flex-box">
                                 <button className="card-button">Отклонить</button>
