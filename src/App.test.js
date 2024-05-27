69bb0ad960ceeb8f1b18ff2ad85187b2bb78a8c9
APUPUS <133990680+potatovich@users.noreply.github.com>
2024-05-27 22:07:41 +0500

    added important comment

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn react link', () => { //метод для тестирования, хрен знает зачем
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
